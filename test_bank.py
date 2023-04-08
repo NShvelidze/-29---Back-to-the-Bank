1ce815381ec9a5cf3475e0d0c5dc84c6130036a1
Nika Shvelidze <117501895+NShvelidze@users.noreply.github.com>
2023-04-09 02:10:39 +0400

    Update test_bank.py

--- test_bank.py
+++ test_bank.py
@@ -3,12 +3,10 @@ from bank import value
 def main():
     test_amount()
 
-
 def test_amount():
     assert value("Hello") == 0
     assert value("H") == 20
     assert value("Ola") == 100
-
 
 if __name__ == "__main__":
     main()
