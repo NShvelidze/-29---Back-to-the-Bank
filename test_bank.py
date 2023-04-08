699d5a9f1a919aaf0a4e504d07bc22407ab0c983
Nika Shvelidze <117501895+NShvelidze@users.noreply.github.com>
2023-04-09 02:09:48 +0400

    Create test_bank.py

---
+++ test_bank.py
@@ -0,0 +1,14 @@
+from bank import value
+
+def main():
+    test_amount()
+
+
+def test_amount():
+    assert value("Hello") == 0
+    assert value("H") == 20
+    assert value("Ola") == 100
+
+
+if __name__ == "__main__":
+    main()
