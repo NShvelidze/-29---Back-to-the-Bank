ebb030bedba36961ec5dc4da7b129253086a1504
Nika Shvelidze <117501895+NShvelidze@users.noreply.github.com>
2023-04-09 02:09:33 +0400

    Create bank.py

---
+++ bank.py
@@ -0,0 +1,17 @@
+def main():
+    greeting = input("Greeting: ").title()
+    amount = value(greeting)
+    print(f"${amount}")
+
+
+
+def value(greeting):
+    if greeting.startswith("Hello"):
+        return 0
+    elif greeting.startswith("H"):
+        return 20
+    else:
+        return 100
+
+if __name__ == "__main__":
+    main()
